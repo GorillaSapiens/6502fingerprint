92f91faea89a2230213b1a9b159e35709188ea82
Gorilla Sapiens <embracetheape@gmail.com>
2025-10-28 23:47:16 -0700

    initial check in

---
+++ fingerprint.asm
@@ -0,0 +1,134 @@
+; ===== ENTRY POINT =====
+START
+    JSR RUN_ARR_FINGERPRINT
+    JMP *
+
+; ===== ZERO PAGE SCRATCH =====
+RESA    = $00
+RESP    = $01
+CRC0    = $02
+CRC1    = $03
+CRC2    = $04
+
+; ===== SAVE_P: store NVZC(P') masked =====
+SAVE_P
+    PHP
+    PLA
+    AND #$C3
+    STA RESP
+    RTS
+
+; ===== CRC-24/OPENPGP =====
+CRC24_INIT
+    LDA #$CE
+    STA CRC0
+    LDA #$04
+    STA CRC1
+    LDA #$B7
+    STA CRC2
+    RTS
+
+CRC24_FEED
+    EOR CRC2
+    STA CRC2
+    LDX #$08
+CRC24_FEED_BIT
+    LDA CRC2
+    AND #$80
+    BEQ CRC24_SHIFT_ONLY
+    ASL CRC0
+    ROL CRC1
+    ROL CRC2
+    LDA CRC0
+    EOR #$FB
+    STA CRC0
+    LDA CRC1
+    EOR #$4C
+    STA CRC1
+    LDA CRC2
+    EOR #$86
+    STA CRC2
+    JMP CRC24_NEXT
+CRC24_SHIFT_ONLY
+    ASL CRC0
+    ROL CRC1
+    ROL CRC2
+CRC24_NEXT
+    DEX
+    BNE CRC24_FEED_BIT
+    RTS
+
+; ===== PROBE #1 =====
+PROBE1
+    CLD
+    SEC
+    LDA #$E5
+    .byte $6B
+    .byte $B8
+    STA RESA
+    JSR SAVE_P
+    RTS
+
+; ===== PROBE #2 =====
+PROBE2
+    CLD
+    SEC
+    LDA #$36
+    .byte $6B
+    .byte $6B
+    STA RESA
+    JSR SAVE_P
+    RTS
+
+; ===== PROBE #3 =====
+PROBE3
+    SED
+    CLC
+    LDA #$F6
+    .byte $6B
+    .byte $6B
+    STA RESA
+    JSR SAVE_P
+    RTS
+
+; ===== PROBE #4 =====
+PROBE4
+    CLD
+    CLC
+    LDA #$06
+    .byte $6B
+    .byte $6B
+    STA RESA
+    JSR SAVE_P
+    RTS
+
+; ===== MAIN =====
+RUN_ARR_FINGERPRINT
+    JSR CRC24_INIT
+
+    JSR PROBE1
+    LDA RESA
+    JSR CRC24_FEED
+    LDA RESP
+    JSR CRC24_FEED
+
+    JSR PROBE2
+    LDA RESA
+    JSR CRC24_FEED
+    LDA RESP
+    JSR CRC24_FEED
+
+    JSR PROBE3
+    LDA RESA
+    JSR CRC24_FEED
+    LDA RESP
+    JSR CRC24_FEED
+
+    JSR PROBE4
+    LDA RESA
+    JSR CRC24_FEED
+    LDA RESP
+    JSR CRC24_FEED
+
+    RTS
+
